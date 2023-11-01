ef5755218c324cf741513ba6e557f80f74707731
Lee-jr-81 <129674277+Lee-jr-81@users.noreply.github.com>
2023-11-01 19:54:54 +0000

    Created the Dragbox component

---
+++ src/components/DragBox.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const DragBox = () => {
+  return (
+    <section className="main-dragbox-container">
+      <div className="dragbox-container">
+        <h1>Hello World</h1>
+      </div>
+    </section>
+  );
+};
+
+export default DragBox;
