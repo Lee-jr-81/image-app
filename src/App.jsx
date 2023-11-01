ab6694183ce18d7c742caf232f79e93ce61b483d
Lee-jr-81 <129674277+Lee-jr-81@users.noreply.github.com>
2023-11-01 17:41:29 +0000

    Initial setup of files and linked to this repo

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <main className="background">
+      <h1 className="main_contain">Hello world</h1>
+    </main>
+  );
+}
+
+export default App;
