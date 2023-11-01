9f0137831430cb27de7c2fab321efd6582d64193
steve4098 <139075699+steve4098@users.noreply.github.com>
2023-11-01 20:22:31 +0000

    navbar.jsx edited to add flex class to ul element. app.css edited to include sections for navbar - currently very experimental, colours added for clarity rather than asthetics - will be edited from here

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,17 +5,17 @@ import { BrowserRouter, Route, Link } from "react-router-dom";
 function navbar() {
     return (
         <section>
-            <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <nav class="navbar navbar-expand-lg ">
                 <a class="navbar-brand" href="#"> 
                 {/* <></> <-- put logo image link here*/}
                 Minify</a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
-                <div class="collapse navbar-collapse" id="navbarNav">
+                <div class="collapse navbar-collapse d-flex justify-content-evenly" id="navbarNav">
                     <ul class="navbar-nav">
-                        <li class="nav-item active">
-                            <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Home</a>
                         </li>
                         <li class="nav-item">
                             <a class="nav-link" href="#">Go Premium!</a>
