9b4f7651b0de4cfc64cdd4b973c293c88e0e8a18
Lee-jr-81 <129674277+Lee-jr-81@users.noreply.github.com>
2023-11-02 20:03:03 +0000

    Updated the dragbox component for better resposiveness

--- src/components/DragBox.jsx
+++ src/components/DragBox.jsx
@@ -2,11 +2,11 @@ import React from "react";
 
 export default function DragBox() {
   return (
-    <section class="container-sm">
+    <section class="container">
       <div className="dragbox-container">
         <div className="Dashed-container">
           <h1 className="title">Drag Your Images Or Click To Browse</h1>
-          <button>Upload</button>
+          <button className="upload-button">Upload</button>
         </div>
       </div>
     </section>
