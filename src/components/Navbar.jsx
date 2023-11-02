c7ced2ed6c180a89fcb57a4fe63bf4d2ea011d32
steve4098 <139075699+steve4098@users.noreply.github.com>
2023-11-02 21:23:53 +0000

    app.css edited to format layout of navbar. navbar.jsx edited to collapse correctly

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,45 +4,33 @@ import React from "react";
 export default function navbar() {
   return (
     <section>
-      <nav class="navbar navbar-expand-lg">
-        <a class="navbar-brand" href="#">
-          {/* <></> <-- put logo image link here*/}
+
+      <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <a className="navbar-brand" href="#">
+          <img src="../assets/logo-small.jpg" />
           Minify
         </a>
-        <button
-          class="navbar-toggler"
-          type="button"
-          data-toggle="collapse"
-          data-target="#navbarNav"
-          aria-controls="navbarNav"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
-        <div
-          class="collapse navbar-collapse d-flex justify-content-evenly"
-          id="navbarNav"
-        >
+        <div class="collapse navbar-collapse" id="navbarNavDropdown">
           <ul class="navbar-nav">
-            <li class="nav-item">
-              <a class="nav-link" href="#">
-                Home
-              </a>
+            <li class="nav-item active">
+              <a class="nav-link" href="#">Home</a>
             </li>
             <li class="nav-item">
-              <a class="nav-link" href="#features">
-                Features
-              </a>
+              <a class="nav-link" href="#features">Features</a>
             </li>
             <li class="nav-item">
-              <a class="nav-link" href="#">
-                Log In
-              </a>
+              <a class="nav-link" href="#exampleModal" data-toggle="modal" data-target="#exampleModal">Log In</a>
             </li>
+
           </ul>
         </div>
       </nav>
+
     </section>
   );
 }
+
+
