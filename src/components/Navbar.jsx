10c9a2dba0cf11763b228add2d3ec78c1cf67c0e
steve4098 <139075699+steve4098@users.noreply.github.com>
2023-11-07 18:58:28 +0000

    logo image added to navbar through constant

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Logo from "../assets/logo-small.jpg";
 
 
 export default function navbar() {
@@ -7,7 +8,7 @@ export default function navbar() {
 
       <nav class="navbar navbar-expand-lg navbar-light bg-light">
         <span className="navbar-brand" href="#">
-          <img src="../assets/logo-small.jpg" width="30" height="30"/>
+          <img src={Logo} width="40" height="40" class="d-inline-block align-top"/>  
           Minify
         </span>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
