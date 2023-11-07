525d4516164fc54d4eefa87f9c1e78585c645e40
steve4098 <139075699+steve4098@users.noreply.github.com>
2023-11-07 21:20:40 +0000

    bootstrap code for navbar changed to react-bootstrap code

--- src/main.jsx
+++ src/main.jsx
@@ -9,6 +9,7 @@ import App from "./App.jsx";
 import "bootstrap/dist/css/bootstrap.css";
 import "./index.css";
 
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
