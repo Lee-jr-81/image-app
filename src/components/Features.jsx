c5b00301d4f9badf106c6d4fc02e04e08bc70ef8
Mamunur Rahman <mamunur@mac-air.home>
2023-11-07 10:18:41 +0000

    footer component added with gradient colour, tagline added to features section

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -3,6 +3,21 @@ import React from "react";
 export default function Banner() {
   return (
     <section class="features-container"id="features">
+
+
+ 
+    <div class="container text-center">
+      <div class="row justify-content-center">
+        <div class="col-md-7">
+          <h2 class="fw-light mt-5 text-white">Why Minify?</h2>
+          <p class="lead text-white-50">Use the best compression tool for all your websites and apps!</p>
+        </div>
+      </div>
+    </div>
+  
+  
+
+
       <div class="features-wrapper">
         <h1 class="features-title">
          Features 
@@ -12,7 +27,7 @@ export default function Banner() {
      <div class="feat-row">
         
         <div class="feat-text">
-            <h3 class=""><b>Smaller</b> images, <b>Faster</b> websites</h3><p class="">Websites images are usually too big and unoptimized for web purposes. It is easy to save a few megabytes by compressing your assets without noticing any loss in quality.</p>
+            <h3 class=""><b>Smaller</b> images, <b>Faster</b> websites</h3><p class="lead text-white">Websites images are usually too big and unoptimized for web purposes. It is easy to save a few megabytes by compressing your assets without noticing any loss in quality.</p>
         </div>
         
         <div class="feat-img">
---
+++ src/components/Footer.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+   
+    <div class="d-flex flex-column">
+  
+  <footer id="sticky-footer" class="flex-shrink-0 py-4 bg-dark text-white-50">
+    <div class="container text-center">
+      <small>Minify &copy; 2023</small>
+    </div>
+  </footer>
+</div>
+
+  );
+}
