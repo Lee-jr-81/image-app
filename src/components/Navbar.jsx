8f6f51c8d1be858d01013d49dff6c0ea52f465e7
steve4098 <139075699+steve4098@users.noreply.github.com>
2023-11-07 21:34:11 +0000

    navbar.jsx tidied up and modal imported from react-bootstrap

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,9 +3,13 @@ import Logo from "../assets/logo-small.jpg";
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import Modal from "react-bootstrap/Modal";
 
 
 export default function navbar() {
+
+ 
+
   return (
     <Navbar expand="lg" className="bg-body-tertiary navbar">
       <Container className="display-class">
@@ -17,7 +21,7 @@ export default function navbar() {
             <Nav.Link href="#">Home</Nav.Link>
             <Nav.Link href="#features">Features</Nav.Link>
             <Nav.Link href="#compressor">Compress</Nav.Link>
-            <Nav.Link href="#logIn">Log In</Nav.Link>
+            <Nav.Link href="#">Log In</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
@@ -26,52 +30,3 @@ export default function navbar() {
 }
 
 
-
-
-
-
-
-
-
-
-
-// export default function navbar() {
-
-//   return (
-
-
-
-
-    
-//     <section>
-
-//       <nav class="navbar navbar-expand-lg navbar-light bg-light">
-//         <a className="navbar-brand" href="#">
-//           <img src={Logo} width="40" height="40" class="d-inline-block align-center logo" />
-//           Minify
-//         </a>
-//         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-//           <span class="navbar-toggler-icon"></span>
-//         </button>
-//         <div class="collapse navbar-collapse" id="navbarNavDropdown">
-//           <ul class="navbar-nav">
-//             <li class="nav-item active">
-//               <a class="nav-link" href="#">Home</a>
-//             </li>
-//             <li class="nav-item">
-//               <a class="nav-link" href="#features">Features</a>
-//             </li>
-//             <li class="nav-item">
-//               <a class="nav-link" href="#compressor">Compress</a>
-//             </li>
-//             <li class="nav-item">
-//               <a class="nav-link" href="">Log In</a>
-//             </li>
-//           </ul>
-//         </div>
-//       </nav>
-
-//     </section>
-//   );
-
-// }
